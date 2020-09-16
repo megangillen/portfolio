68716cac7803542a2a01c474fe13c336a2314614
mgillen17 <70616677+mgillen17@users.noreply.github.com>
2020-09-15 23:35:26 -0400

    Update with hamburger icon & up arrow

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 console.log("Hi there! Welcome to my Portfolio Site.")
+
+function menuToggle() {
+  var x = document.getElementById('navigation-bar');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
