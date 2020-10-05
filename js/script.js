9da9bb9c6c4dd94ee5edf2c81544da60e5950e53
mgillen17 <70616677+mgillen17@users.noreply.github.com>
2020-10-04 20:26:22 -0400

    Update footer icons, add links to images

--- js/script.js
+++ js/script.js
@@ -12,7 +12,9 @@ function menuToggle() {
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction()
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
